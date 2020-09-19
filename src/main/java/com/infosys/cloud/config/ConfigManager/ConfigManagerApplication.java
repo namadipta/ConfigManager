925711a85f456b854c98bb8f1b6247f2f6b4ad0b
namadipta <namadipta@bs-macbook-pro.dlink>
2020-09-19 20:08:56 +0530

    initial Backend

--- src/main/java/com/infosys/cloud/config/ConfigManager/ConfigManagerApplication.java
+++ src/main/java/com/infosys/cloud/config/ConfigManager/ConfigManagerApplication.java
@@ -2,8 +2,10 @@ package com.infosys.cloud.config.ConfigManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.infosys" })
 public class ConfigManagerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/infosys/cloud/config/ConfigManager/ui/HomePageController.java
+++ src/main/java/com/infosys/cloud/config/ConfigManager/ui/HomePageController.java
@@ -1,20 +1,37 @@
-package com.infosys.cloud.config.ConfigManager.ui;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-/**
- * @author namadipta
- *
- */
-@Controller
-public class HomePageController {
-
-	@RequestMapping(value = "/")
-	public String index(ModelMap model) {
-		model.addAttribute("content", "dashBoardDynamic");
-		return "index";
-	}
-
-}
+//package com.infosys.cloud.config.ConfigManager.ui;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+///**
+// * @author namadipta
+// *
+// */
+//@Controller
+//public class HomePageController {
+//
+//	@RequestMapping(value = "/")
+//	public String index(ModelMap model) {
+//		model.addAttribute("content", "dynamicDashBoard");
+////		 HashMap<String,String> listOfModule= new HashMap<>();
+////		 listOfModule.put("test","test1");
+//		List <String> listOfModule = new ArrayList<>();
+//		listOfModule.add("test");
+//		listOfModule.add("test1");
+//		DropdownValue d = new DropdownValue("test", "test");
+//		DropdownValue d1 = new DropdownValue("test1", "test1");
+//		Modules m = new Modules();
+//		m.setListOfModule(listOfModule);
+//		List<DropdownValue> listOfModules =  new ArrayList<>();
+//		listOfModules.add(d);
+//		listOfModules.add(d1);
+//		m.setListOfModules(listOfModules);
+//		model.addAttribute("modules", m);
+//		return "index";
+//	}
+//
+//}
