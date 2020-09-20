190c0bcea85f4e9bbcce519c4381dca3ec79edf5
B namadipta Patro <namadipta@Bs-MacBook-Pro.Dlink>
2020-09-20 22:04:16 +0530

    first page

--- src/main/java/com/infosys/cloud/config/ConfigManager/controller/display/controller/HomePageController.java
+++ src/main/java/com/infosys/cloud/config/ConfigManager/controller/display/controller/HomePageController.java
@@ -5,13 +5,24 @@ package com.infosys.cloud.config.configmanager.controller.display.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.infosys.cloud.config.configmanager.model.display.AppDisplayDetails;
+import com.infosys.cloud.config.configmanager.model.display.AppsDisplayRequest;
 import com.infosys.cloud.config.configmanager.model.service.AppDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.EnvDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.ModuleDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.ProfDetailsServicePojo;
 import com.infosys.cloud.config.configmanager.service.AppDetailsService;
+import com.infosys.cloud.config.configmanager.service.EnvDetailsService;
+import com.infosys.cloud.config.configmanager.service.ModuleDetailsService;
+import com.infosys.cloud.config.configmanager.service.ProfDetailsService;
 import com.infosys.cloud.config.configmanager.service.display.service.HomePageService;
 
 /**
@@ -27,10 +38,47 @@ public class HomePageController {
 	@Autowired
 	private HomePageService homePageService;
 
-	@RequestMapping(value = "/appdetails")
+	@Autowired
+	private EnvDetailsService envDetailsService;
+
+	@Autowired
+	private ModuleDetailsService moduleDetailsService;
+
+	@Autowired
+	private ProfDetailsService profDetailsService;
+
+	@GetMapping(value = "/apps")
 	public String index(ModelMap model) {
 		List<AppDetailsServicePojo> listofApp = appDetailsService.fetchAllAppDetails();
-		model.addAttribute("listOfApps", homePageService.convertAppDetailsForDropDown(listofApp));
+		AppsDisplayRequest modelData = new AppsDisplayRequest();
+		modelData.setListOfApps(homePageService.convertAppDetailsForDropDown(listofApp));
+		model.addAttribute("appData", modelData);
 		return "appselector";
+	}
+
+	@PostMapping(value = "/appdetails")
+	public String loadAppDetails(ModelMap model, @ModelAttribute("appName") AppsDisplayRequest selectedAppRequest) {
+		String selectedAppId = selectedAppRequest.getAppName();
+		AppDisplayDetails appDisplayDetails = new AppDisplayDetails();
+		if (StringUtils.isNotBlank(selectedAppId)) {
+			List<EnvDetailsServicePojo> envDetails = envDetailsService
+					.fetchAllEnvForAppID(Long.parseLong(selectedAppId));
+			appDisplayDetails.setListOfEnvs(homePageService.convertEnvDetailsForDropDown(envDetails));
+		}
+
+		if (StringUtils.isNotBlank(selectedAppId)) {
+			List<ModuleDetailsServicePojo> moduleDetails = moduleDetailsService
+					.fetchAllModuleForAppID(Long.parseLong(selectedAppId));
+			appDisplayDetails.setListOfModules(homePageService.convertModuleDetailsForDropDown(moduleDetails));
+		}
+
+		if (StringUtils.isNotBlank(selectedAppId)) {
+			List<ProfDetailsServicePojo> profDetails = profDetailsService
+					.fetchAllProfForAppID(Long.parseLong(selectedAppId));
+			appDisplayDetails.setListOfProfs(homePageService.convertProfDetailsForDropDown(profDetails));
+		}
+		model.addAttribute("appDisplayDetails", appDisplayDetails);
+		model.addAttribute("content", "appDetails");
+		return "index";
 	}
 }
--- src/main/java/com/infosys/cloud/config/ConfigManager/mapper/display/mapper/CommonMapper.java
+++ src/main/java/com/infosys/cloud/config/ConfigManager/mapper/display/mapper/CommonMapper.java
@@ -13,6 +13,9 @@ import org.mapstruct.NullValueMappingStrategy;
 
 import com.infosys.cloud.config.configmanager.model.common.DropdownValue;
 import com.infosys.cloud.config.configmanager.model.service.AppDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.EnvDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.ModuleDetailsServicePojo;
+import com.infosys.cloud.config.configmanager.model.service.ProfDetailsServicePojo;
 
 /**
  * @author namadipta
@@ -30,5 +33,35 @@ public interface CommonMapper {
 
 	@Mappings({ @Mapping(source = "appId", target = "id"), @Mapping(source = "appName", target = "text") })
 	DropdownValue mapForDropdowns(AppDetailsServicePojo appDetails);
+
+	/**
+	 * @param envDetails
+	 * @return
+	 */
+	@IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+	List<DropdownValue> mapForEnvDropdown(List<EnvDetailsServicePojo> envDetails);
+
+	@Mappings({ @Mapping(source = "envId", target = "id"), @Mapping(source = "envDisplayName", target = "text") })
+	DropdownValue mapForDropdowns(EnvDetailsServicePojo appDetails);
+
+	/**
+	 * @param moduleDetails
+	 * @return
+	 */
+	@IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+	List<DropdownValue> mapForModule(List<ModuleDetailsServicePojo> moduleDetails);
+
+	@Mappings({ @Mapping(source = "moduleId", target = "id"), @Mapping(source = "moduleDisplayName", target = "text") })
+	DropdownValue mapForModuleDropdowns(ModuleDetailsServicePojo moduleDetails);
+
+	/**
+	 * @param profDetails
+	 * @return
+	 */
+	@IterableMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+	List<DropdownValue> mapForProf(List<ProfDetailsServicePojo> profDetails);
+
+	@Mappings({ @Mapping(source = "profId", target = "id"), @Mapping(source = "profDisplayName", target = "text") })
+	DropdownValue mapForModuleDropdowns(ProfDetailsServicePojo moduleDetails);
 
 }
