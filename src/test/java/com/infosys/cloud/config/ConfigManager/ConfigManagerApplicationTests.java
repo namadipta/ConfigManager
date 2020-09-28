012e2562fd7a68df8dfe425fe6d79ebda91441e4
B namadipta Patro <namadipta@Bs-MacBook-Pro.local>
2020-09-29 00:06:48 +0530

    bulk upload

--- src/test/java/com/infosys/cloud/config/ConfigManager/ConfigManagerApplicationTests.java
+++ src/test/java/com/infosys/cloud/config/ConfigManager/ConfigManagerApplicationTests.java
@@ -1,4 +1,4 @@
-//package com.infosys.cloud.config.configmanager;
+//package com.cloud.config.configmanager;
 //
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
